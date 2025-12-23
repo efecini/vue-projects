f35f4aca299a952c359da2f920d6291bc9fd0cf1
efecini <efecini@gmail.com>
2025-12-23 03:15:02 +0300

    add sidebar

--- deezer-clone/src/App.vue
+++ deezer-clone/src/App.vue
@@ -1,27 +1,77 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Magnify from "vue-material-design-icons/Magnify.vue"
-import Bell from "vue-material-design-icons/Bell.vue"
+import HelloWorld from './components/HelloWorld.vue';
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import Bell from 'vue-material-design-icons/Bell.vue';
 </script>
 
 <template>
-	<div
-		id = "TopNav"
-		class= "fixed right-0 flex item s-center justify-between w-[calc(100%-240px)] h-[56px] border-b border-b-[#32323D]"
-	>
-	<div class ="flex items-center w-full">
-		<Magnify class="pl-6 mt-1 pr-2" fillColor="#7E7E88" :size="22" />
-		<input class=" p-1 bg-transparent outline-none font-[300] placeholder-[#BEBEC7] text-[#FFFFFF] w-full max-w-xl" placeholder="Search" type ="text" />
-	</div>
-	<div class="flex items-center pr-10">
-		<div class="mr-4 p-1 hover:bg-gray-600 rounded-full cursor-pointer">
-			<Bell fillColor="#FFFFFF" :size="20"/>
-		</div>
-		<img
-		class="rounded-full w-[33px]"
-		src="https://yt3.ggpht.com/yti/ANjgQV9IisS8pSBrKpXYX9AhHycjG2iXcypkFbvoR022-ZYXZew=s88-c-k-c0x00ffffff-no-rj"
-		>
-	</div>
-	</div>
-  <HelloWorld/>
+  <div
+    id="TopNav"
+    class="fixed right-0 flex item s-center justify-between w-[calc(100%-0px)] h-[56px] border-b border-b-[#32323D]"
+  >
+    <div class="flex items-center w-full">
+      <Magnify class="pl-6 mt-1 pr-2" fillColor="#7E7E88" :size="22" />
+      <input
+        class="p-1 bg-transparent outline-none font-[300] placeholder-[#BEBEC7] text-[#FFFFFF] w-full max-w-xl"
+        placeholder="Search"
+        type="text"
+      />
+    </div>
+    <div class="flex items-center pr-10">
+      <div class="mr-4 p-1 hover:bg-gray-600 rounded-full cursor-pointer">
+        <Bell fillColor="#FFFFFF" :size="20" />
+      </div>
+      <img
+        class="rounded-full w-[33px]"
+        src="https://yt3.ggpht.com/yti/ANjgQV9IisS8pSBrKpXYX9AhHycjG2iXcypkFbvoR022-ZYXZew=s88-c-k-c0x00ffffff-no-rj"
+      />
+    </div>
+
+    <div
+      id="SideNav"
+      class="fixed w-[240px] bg-[#191922] h-[100vh] border-r border-r-[#32323D]"
+    >
+      <div class="w-full pl-6 pt-3 cursor-pointer">
+        <RouterLink to="/"> </RouterLink>
+      </div>
+
+      <div class="mt-[53px]"></div>
+
+      <SideMenuItem
+        iconString="music"
+        :iconSize="20"
+        pageUrl="/"
+        name="Music"
+      />
+      <SideMenuItem
+        iconString="podcast"
+        :iconSize="20"
+        pageUrl="/podcasts"
+        name="Podcasts"
+      />
+      <SideMenuItem
+        iconString="explore"
+        :iconSize="20"
+        pageUrl="/artist"
+        name="Explore"
+      />
+      <SideMenuItem
+        iconString="favourite"
+        :iconSize="20"
+        pageUrl="/favourite"
+        name="Favourites"
+      />
+
+      <div class="text-[#A2A2AD] font-light text-[12px] pl-[62px] mt-[25px]">
+        <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">
+          Favourite tracks
+        </div>
+        <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Playlist</div>
+        <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Albums</div>
+        <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Artists</div>
+        <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Podcasts</div>
+      </div>
+    </div>
+  </div>
+  <HelloWorld />
 </template>
